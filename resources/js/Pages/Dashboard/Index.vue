7e77df3f3258d8c33f18d6b052228ce457455a5d
philipsenwout <philipsenwout@outlook.com>
2020-05-12 18:33:16 +0200

    01. Laravel with Inertia JS set up for a new project

---
+++ resources/js/Pages/Dashboard/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>This is the dashboard, welcome {{ fname }} {{ lname }}</p>
+    </div>
+</template>
+
+<<script>
+export default {
+    props: [
+        'fname', 'lname',
+    ]
+}
+</script>
